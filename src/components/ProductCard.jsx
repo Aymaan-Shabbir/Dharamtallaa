fe0d927d35360fc4dae672689ec7a38a4f8cdaf0
Aymaan-Shabbir <aymaanshabbir27@gmail.com>
2025-02-20 20:09:53 +0530

    dynamic routing added

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,19 +1,29 @@
 /* eslint-disable react/prop-types */
+import { Link } from "react-router-dom";
+
 export const ProductCard = ({ product }) => {
   return (
-    <div className="w-80 flex flex-col items-center justify-center border p-5 bg-white rounded-lg shadow-lg transition duration-300 hover:scale-105">
+    <div className=" text-center bg-white h-full border rounded-lg shadow-lg p-4 transition-transform transform hover:scale-104 m-2">
       <img
         src={product.image}
         alt={product.title}
-        className="w-40 h-40 object-contain border rounded-lg"
+        className="w-full h-40 object-contain rounded-md"
       />
-      <h2 className="text-lg font-bold mt-3 text-gray-800 text-center">
+      <h3 className="text-lg font-semibold mt-2 text-gray-900">
         {product.title}
-      </h2>
-      <p className="text-md font-semibold text-orange-600 mt-1">
-        Rating: {product.rating.rate}
+      </h3>
+
+      <p className="text-orange-600 font-bold">
+        Rating: {product.rating?.rate || "N/A"}
       </p>
-      <p className="text-lg font-bold text-blue-700 mt-1">₹{product.price}</p>
+      <p className="text-blue-700 font-bold">₹{product.price}</p>
+      <Link
+        key={product.id}
+        to={`/product/${product.id}`}
+        className="block bg-black text-white text-center px-4 py-2 mt-3 rounded-lg transition duration-300 hover:bg-gray-800"
+      >
+        View Details
+      </Link>
     </div>
   );
 };
